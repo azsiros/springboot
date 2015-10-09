56a2170ac749e54fd6bfc3d8a7642f506e272793
Anton Zsiros <azsiros@gratex.com>
2015-10-09 16:47:28 +0200

    correction in properties pointing to truststore

--- rssreader/src/main/java/com/gratex/rssreader/blog/GtiCreadentialSupplier.java
+++ rssreader/src/main/java/com/gratex/rssreader/blog/GtiCreadentialSupplier.java
@@ -2,14 +2,24 @@ package com.gratex.rssreader.blog;
 
 import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import com.rometools.fetcher.impl.HttpClientFeedFetcher.CredentialSupplier;
 
+@Component
 public class GtiCreadentialSupplier implements CredentialSupplier{
-
+	
+	
+	@Value("${username}")
+	private String userName;
+	
+	@Value("${pwd}")
+	private String pwd;
+	
 	@Override
 	public Credentials getCredentials(String realm, String host) {
-		return new UsernamePasswordCredentials("azsiros", "");
+		return new UsernamePasswordCredentials(userName, pwd);
 	}
 
 }
